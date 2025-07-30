64eb1074f150da8f76a6ce48fb81d3713dae5613
Michael Milton <michael.r.milton@gmail.com>
2025-07-30 16:55:51 +1000

    Use dataclass for test tuples, add a missing annotation test case

--- test/test_merge.py
+++ test/test_merge.py
@@ -1,132 +1,161 @@
 import ast
 import pytest
+from dataclasses import dataclass
 from stubmerge import merge_annotations
+
+@dataclass
+class MergeCase:
+    src: str
+    annot: str
+    expected: str
 
 def ast_equal(a: ast.AST, b: ast.AST):
     return ast.dump(a, annotate_fields=True) == ast.dump(b, annotate_fields=True)
 
 @pytest.mark.parametrize(
-    "src, annot, expected",
+    "case",
     [
-        # Return type override
-        ("""
+        MergeCase(
+            src="""
 from typing import Any
 
 def foo(x: int) -> Any:
     return x
 """,
-         """
+            annot="""
 from typing import Any
 
 def foo(x: int) -> int:
     return x
 """,
-         """
+            expected="""
 from typing import Any
 
 def foo(x: int) -> int:
     return x
-"""),
-        # Do not override with Any
-        ("""
+"""
+        ),
+        MergeCase(
+            src="""
 from typing import Any
 
 def bar(x: int) -> int:
     return x
 """,
-         """
+            annot="""
 from typing import Any
 
 def bar(x: int) -> Any:
     return x
 """,
-         """
+            expected="""
 from typing import Any
 
 def bar(x: int) -> int:
     return x
-"""),
-        # Argument type override
-        ("""
+"""
+        ),
+        MergeCase(
+            src="""
 from typing import Any
 
 def baz(x: Any) -> int:
     return 42
 """,
-         """
+            annot="""
 from typing import Any
 
 def baz(x: str) -> int:
     return 42
 """,
-         """
+            expected="""
 from typing import Any
 
 def baz(x: str) -> int:
     return 42
-"""),
-        # Union types
-        ("""
+"""
+        ),
+        MergeCase(
+            src="""
 from typing import Any
 
 def foo(x: int) -> Any | int:
     return x
 """,
-         """
+            annot="""
 from typing import Any
 
 def foo(x: int) -> int | str:
     return x
 """,
-         """
+            expected="""
 from typing import Any
 
 def foo(x: int) -> int | str:
     return x
-"""),
-        # Incomplete
-        ("""
+"""
+        ),
+        MergeCase(
+            src="""
 from _typeshed import Incomplete
 
 def foo(x: Incomplete) -> Incomplete:
     return x
 """,
-         """
+            annot="""
 from _typeshed import Incomplete
 
 def foo(x: int) -> int:
     return x
 """,
-         """
+            expected="""
 from _typeshed import Incomplete
 
 def foo(x: int) -> int:
     return x
-"""),
-        # Union with Incomplete
-        ("""
+"""
+        ),
+        MergeCase(
+            src="""
 from _typeshed import Incomplete
 
 def foo(x: int | Incomplete) -> Incomplete | str:
     return x
 """,
-         """
+            annot="""
 from _typeshed import Incomplete
 
 def foo(x: int | str) -> str:
     return x
 """,
-         """
+            expected="""
 from _typeshed import Incomplete
 
 def foo(x: int | str) -> str:
     return x
-"""),
+"""
+        ),
+        MergeCase(
+            src="""
+def foo(x):
+    return x
+""",
+            annot="""
+from typing import Any
+
+def foo(x: Any):
+    return x
+""",
+            expected="""
+def foo(x: Any):
+    return x
+"""
+        ),
     ]
 )
-def test_merge_annotations_param(src: str, annot: str, expected: str):
-    src_ast = ast.parse(src)
-    annot_ast = ast.parse(annot)
-    expected_ast = ast.parse(expected)
+def test_merge_annotations_param(case: MergeCase):
+    src_ast = ast.parse(case.src)
+    annot_ast = ast.parse(case.annot)
+    expected_ast = ast.parse(case.expected)
     merged = merge_annotations(src_ast, annot_ast)
     assert ast_equal(merged, expected_ast)
