02b63da2cea1ba07b272cf4d7ef17739ab94fdb9
Michael Milton <michael.r.milton@gmail.com>
2025-07-30 16:38:11 +1000

    Cleanup slop

--- src/stubmerge/test_visitor.py
+++ src/stubmerge/test_visitor.py
@@ -1,6 +1,5 @@
 import ast
 from stubmerge.visitor import is_more_informative_type
-from stubmerge import merge_annotations
 
 def parse_type(expr: str):
     return ast.parse(expr, mode="eval").body
@@ -38,71 +37,3 @@ def test_union_placeholders_vs_real_type():
     assert is_more_informative_type(parse_type("str"), parse_type("Any | Incomplete"))
     assert not is_more_informative_type(parse_type("Any | Incomplete"), parse_type("int | str"))
     assert is_more_informative_type(parse_type("int | str"), parse_type("Any | Incomplete"))
-
-def get_func_annotation(module_ast, func_name):
-    for node in module_ast.body:
-        if isinstance(node, ast.FunctionDef) and node.name == func_name:
-            return node.returns
-    return None
-
-def test_merge_annotations_overrides_return_type():
-    src = """
-from typing import Any
-
-def foo(x: int) -> Any:
-    return x
-"""
-    annot = """
-from typing import Any
-
-def foo(x: int) -> int:
-    return x
-"""
-    src_ast = ast.parse(src)
-    annot_ast = ast.parse(annot)
-    merged = merge_annotations(src_ast, annot_ast)
-    ret = get_func_annotation(merged, "foo")
-    assert isinstance(ret, ast.Name)
-    assert ret.id == "int"
-
-def test_merge_annotations_does_not_override_with_any():
-    src = """
-from typing import Any
-
-def bar(x: int) -> int:
-    return x
-"""
-    annot = """
-from typing import Any
-
-def bar(x: int) -> Any:
-    return x
-"""
-    src_ast = ast.parse(src)
-    annot_ast = ast.parse(annot)
-    merged = merge_annotations(src_ast, annot_ast)
-    ret = get_func_annotation(merged, "bar")
-    assert isinstance(ret, ast.Name)
-    assert ret.id == "int"
-
-def test_merge_annotations_argument_type():
-    src = """
-from typing import Any
-
-def baz(x: Any) -> int:
-    return 42
-"""
-    annot = """
-from typing import Any
-
-def baz(x: str) -> int:
-    return 42
-"""
-    src_ast = ast.parse(src)
-    annot_ast = ast.parse(annot)
-    merged = merge_annotations(src_ast, annot_ast)
-    for node in merged.body:
-        if isinstance(node, ast.FunctionDef) and node.name == "baz":
-            arg = node.args.args[0]
-            assert isinstance(arg.annotation, ast.Name)
-            assert arg.annotation.id == "str"
